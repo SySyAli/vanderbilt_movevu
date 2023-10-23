e2075ff58fe2e86364b173cfcd1f303e2f536f10
SySyAli <syedansabali786@gmail.com>
2023-10-23 00:11:20 -0500

    Removing Python Testing File.

--- testing/testing.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-# Load environment variables from a .env file
-load_dotenv()
-
-# Access environment variables
-TESTING_BUS_CSV = os.environ.get("TESTING_BUS_CSV")
-
